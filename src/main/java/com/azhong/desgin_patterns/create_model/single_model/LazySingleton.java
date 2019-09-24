47979d9446b64536f0eeca0ba70faf9a4ad350d8
zhonghui.Xia <18666122391@163.com>
2019-09-25 02:38:54 +0800

    添加代码实现工厂模式

---
+++ src/main/java/com/azhong/desgin_patterns/create_model/single_model/LazySingleton.java
@@ -0,0 +1,17 @@
+package com.azhong.desgin_patterns.create_model.single_model;
+
+/**
+ * 懒汉模式
+ */
+public class LazySingleton {
+    private static volatile LazySingleton instance=null;//保证 instance在所有线程中同步(可见性）
+    private LazySingleton(){};//避免类在外部实例化
+    //方法上加同步
+    public static synchronized LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+
+        }
+        return instance;
+    }
+}
